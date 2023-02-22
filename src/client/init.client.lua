c60ad01fd72aa795f3dfe95b7be6f34e65e1485e
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2023-02-22 14:58:38 +0000

    Finish testing fusion

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -8,7 +8,10 @@ local Value = Fusion.Value
 local Observer = Fusion.Observer
 local New = Fusion.New
 local Hydrate = Fusion.Hydrate
+local ForValues = Fusion.ForValues
+local ForPairs = Fusion.ForPairs
 
+local names = Value({"prooheckcp", "prooheck", "proo"})
 local color = Value(Color3.fromRGB(100, 100, 100))
 
 local app = New "ScreenGui" {
@@ -18,8 +21,15 @@ local app = New "ScreenGui" {
     {
         Hydrate(Label()){
             BackgroundColor3 = color
-        }
-        
+        },
+
+        ForPairs(names, function(index, value)
+            return index, New "TextLabel" {
+                Size = UDim2.fromScale(1, 0.1),
+                Position = UDim2.fromScale(0, 0.11 * (index - 1)),
+                Text = value
+            }
+        end, Fusion.cleanup)
     }
 
 }
